2d0ada067b487533803c406b3fc48b220a0a8f2a
nootovich <>
2022-07-25 02:16:27 +0300

    restored back my mistake

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class Canvas extends JPanel {
     Canvas() {
-        this.setPreferredSize(new Dimension(Global.width, Global.height));
+        this.setPreferredSize(new Dimension(MainLoop.width, MainLoop.height));
         this.setVisible(true);
     }
 
@@ -11,8 +12,11 @@ public class Canvas extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(Color.BLACK);
         g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
-        for (Tetromino t : Global.tetrominos) {
+        for (Tetromino t : MainLoop.tetrominos) {
             t.show(g2d);
         }
+        g2d.setColor(Color.WHITE);
+        g2d.drawString(Arrays.toString(MainLoop.heldKeys), 10, 10);
+//        g2d.drawString(MainLoop.tetrominos.get(MainLoop.tetrominos.size() - 1).rotation + "", 10, 30);
     }
 }
